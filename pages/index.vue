6da3f472b21d9d389a0c4d629baa375c868f9e9b
Gustavo Waki <wakigustavo@gmail.com>
2024-09-11 23:24:57 -0300

    Refactor index page and add Product component

--- pages/index.vue
+++ pages/index.vue
@@ -2,7 +2,8 @@
   <div>
     <HighlightBar />
     <HeaderPage />
-    <Tutorial class="text-blue-600" />
+    <!-- <Tutorial class="text-blue-600" /> -->
+    <Product />
   </div>
 </template>
 
@@ -10,11 +11,17 @@
 import Vue from 'vue'
 import HighlightBar from '~/components/HighlightBar.vue'
 import HeaderPage from '~/components/HeaderPage.vue'
+<<<<<<< Updated upstream
+=======
+import Product from '~/components/Product.vue'
+>>>>>>> Stashed changes
 
 export default Vue.extend({
   name: 'IndexPage',
   components: {
-    HighlightBar, HeaderPage
+    HighlightBar,
+    HeaderPage,
+    Product,
   },
 })
 </script>
