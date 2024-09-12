276a9afbe76419e4e7aba245e777becafe5441b9
marcotulionasc <marconascpriv@gmail.com>
2024-09-11 22:29:32 -0300

    Co-authored-by: Gustavo Waki <GustavoWakiBarbosa@users.noreply.github.com>

---
+++ components/HighlightBar.vue
@@ -0,0 +1,5 @@
+<template>
+    <div class="bg-color-[#59BEC1] flex">
+        <h3 class="uppercase ">Frete grátis para todo brasil</h3>
+    </div>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,12 @@
+<template>
+  <Tutorial class="text-blue-600"/>
+  <HighlightBar />
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  name: 'IndexPage',
+})
+</script>
