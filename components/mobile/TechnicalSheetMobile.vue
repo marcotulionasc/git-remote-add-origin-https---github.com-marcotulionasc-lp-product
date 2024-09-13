8dd2d25433ee2d4bc3c54c2bf3246a9d164d2114
marcotulionasc <marconascpriv@gmail.com>
2024-09-12 22:03:31 -0300

    :sparkles: adicionando paginas já no index

--- components/mobile/TechnicalSheetMobile.vue
+++ components/mobile/TechnicalSheetMobile.vue
@@ -1,41 +1,4 @@
 <template>
-<div class="h-[723px] px-[627px] pt-[47px] pb-20 bg-[#f0f0f0] flex flex-col justify-start items-start">
-  <!-- Título -->
-  <h2 class="text-4xl font-semibold mb-6 text-center w-full">Ficha Técnica</h2>
-
-  <!-- Tabela com linhas visíveis -->
-  <div class="w-[666px] border border-[#c4c4c4]">
-    <div class="grid grid-cols-2 divide-x divide-[#c4c4c4]">
-
-      <!-- Coluna da Esquerda: Atributos -->
-      <div class="py-2 pl-6">
-        <p class="font-bold text-lg py-2 border-b border-[#c4c4c4]">Marca</p>
-        <p class="font-bold text-lg py-2 border-b border-[#c4c4c4]">Modelo</p>
-        <p class="font-bold text-lg py-2 border-b border-[#c4c4c4]">Cor</p>
-        <p class="font-bold text-lg py-2 border-b border-[#c4c4c4]">Tipo de Lente</p>
-        <p class="font-bold text-lg py-2 border-b border-[#c4c4c4]">Material</p>
-        <p class="font-bold text-lg py-2 border-b border-[#c4c4c4]">Gênero</p>
-        <p class="font-bold text-lg py-2 border-b border-[#c4c4c4]">Tamanho</p>
-        <p class="font-bold text-lg py-2 border-b border-[#c4c4c4]">Medidas</p>
-        <p class="font-bold text-lg py-2">Referência</p>
-      </div>
-
-      <!-- Coluna da Direita: Valores -->
-      <div class="py-2 pl-6">
-        <p class="text-lg py-2 border-b border-[#c4c4c4]">Marca</p>
-        <p class="text-lg py-2 border-b border-[#c4c4c4]">Modelo</p>
-        <p class="text-lg py-2 border-b border-[#c4c4c4]">Cor</p>
-        <p class="text-lg py-2 border-b border-[#c4c4c4]">Tipo de Lente</p>
-        <p class="text-lg py-2 border-b border-[#c4c4c4]">Material</p>
-        <p class="text-lg py-2 border-b border-[#c4c4c4]">Gênero</p>
-        <p class="text-lg py-2 border-b border-[#c4c4c4]">Tamanho</p>
-        <p class="text-lg py-2 border-b border-[#c4c4c4]">Medidas</p>
-        <p class="text-lg py-2">Referência</p>
-      </div>
-      
-    </div>
-  </div>
-</div>
 
 
 
--- pages/index.vue
+++ pages/index.vue
@@ -1,31 +1,43 @@
 <template>
   <div>
-    <HighlightBar />
-    <HeaderPage class="md:block hidden" /> <!-- Versão Desktop -->
-    <!-- <HeaderMobile class="md:hidden"/> Versão Mobile -->
-    <!-- <Tutorial class="text-blue-600" /> -->
-    <Product />
-    <TechnicalSheet />
+    <HighlightBarDesktop class="md:block hidden"/> <!-- Versão Desktop -->
+    <HighlightBarMobile class="md:hidden"/> <!-- Versão Mobile -->
+    <HeaderPageDesktop class="md:block hidden" /> <!-- Versão Desktop -->
+    <HeaderMobile class="md:hidden"/> <!-- Versão Mobile -->   
+    <ProductDesktop class="md:block hidden"/> <!-- Versão Desktop -->
+    <ProductMobile class="md:hidden"/> <!-- Versão Mobile -->
+    <TechnicalSheetDesktop class="md:block hidden"/> <!-- Versão Desktop -->
+    <TechnicalSheetMobile class="md:hidden"/> <!-- Versão Mobile -->
+    
   </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
-import HighlightBar from '~/components/desktop/HighlightBarDesktop.vue'
-import HeaderPage from '~/components/desktop/HeaderPageDesktop.vue'
-import Product from '~/components/desktop/ProductDesktop.vue'
-import TechnicalSheet from '~/components/desktop/TechnicalSheetDesktop.vue'
 
+/* Imports of Desktop Pages */
+
+import HighlightBarDesktop from '~/components/desktop/HighlightBarDesktop.vue'
+import HeaderPageDesktop from '~/components/desktop/HeaderPageDesktop.vue'
+import ProductDesktop from '~/components/desktop/ProductDesktop.vue'
+import TechnicalSheetDesktop from '~/components/desktop/TechnicalSheetDesktop.vue'
+
+/* Imports of Mobile Pages */
+
+import HighlightBarMobile from '~/components/mobile/HighlightBarMobile.vue'
+import HeaderPageMobile from '~/components/mobile/HeaderPageMobile.vue'
+import ProductMobile from '~/components/mobile/ProductMobile.vue'
+import TechnicalSheetMobile from '~/components/mobile/TechnicalSheetMobile.vue'
 
 
 
 export default Vue.extend({
   name: 'IndexPage',
   components: {
-    HighlightBar,
-    HeaderPage,
-    Product,
-    TechnicalSheet,
+    HighlightBarDesktop,
+    HeaderPageDesktop,
+    ProductDesktop,
+    TechnicalSheetDesktop,
   },
 })
 </script>
